5b563bc617dcf630ea976d56d37f997184b7dbdf
0nam1 <damonte@naver.com>
2024-07-02 14:02:35 +0900

    최초 커밋

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,17 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //문자열 String
+        System.out.println("안녕하세요?"); //String
+        System.out.println(12); // 숫자 int
+        System.out.println("12"); // String
+        System.out.println(12 + "12"); //int + String
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+    }
+}
